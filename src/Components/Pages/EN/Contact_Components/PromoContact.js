d27567184c64f0e88918125a7aa968bf753c03aa
AWDS-1998 <awds.0591998@gmail.com>
2022-03-29 21:58:23 +0300

    d

--- src/Components/Pages/EN/Contact_Components/PromoContact.js
+++
@@ -1,8 +0,0 @@
-export default function Promo() {
-    return (
-        // <!-- promo -->
-        <div className="promo contact">
-            <div className="max-container"></div>
-        </div>
-    );
-}
